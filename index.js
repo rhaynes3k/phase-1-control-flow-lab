0139b6e7b306f674d4590cbd79b8fd660adf3af7
rhaynes3k <rhaynes3k@gmail.com>
2021-10-14 22:09:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(dist){
+  if (dist <= 400){
+    return 'This one is on me!'
+  }else if (dist > 2000 && dist < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }else {
+    return 'No can do.'
+  }// Write your code here!
+}
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }// Write your code here!
 }
